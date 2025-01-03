fcdc736c2ac3105fc854329ec7279b9a88e8eb5f
Michael <mquinter88@gmail.com>
2025-01-02 21:53:10 -0500

    wrap up stage 5/5

--- tic-tac-toe/src/TicTacToe.java
+++ tic-tac-toe/src/TicTacToe.java
@@ -2,26 +2,37 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+    private static final String X_WIN = "X wins";
+    private static final String O_WIN = "O wins";
+    private static final String DRAW = "Draw";
+    private static final String GAME_IN_PROGRESS = "Game not finished";
+    private static final String INVALID_STATE = "Impossible";
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        char[][] grid = initializeGrid(sc.nextLine());
+        char[][] grid = {
+                {'_', '_', '_'},
+                {'_', '_', '_'},
+                {'_', '_', '_'}
+        };
 
         printGrid(grid);
-        processPlayerMove(grid, sc);
-        printGrid(grid);
-    }
 
-    private static char[][] initializeGrid(String gameState) {
+        int playerTurn = 1;
+        String gameStatus;
 
-        char[][] grid = new char[3][3];
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                char c = gameState.charAt(row * 3 + col);
-                grid[row][col] = c;
+        while (true) {
+            char currPlayer = playerTurn % 2 == 1 ? 'X' : 'O';
+            processPlayerMove(grid, sc, currPlayer);
+            gameStatus = analyzeGame(grid);
+            printGrid(grid);
+            if (gameStatus.equals(X_WIN) || gameStatus.equals(O_WIN) || gameStatus.equals(DRAW)) {
+                break;
             }
+            playerTurn++;
         }
-        return grid;
+        System.out.println(gameStatus);
     }
 
     private static void printGrid(char[][] grid) {
@@ -37,7 +48,7 @@ public class TicTacToe {
         System.out.println("---------");
     }
 
-    private static void processPlayerMove(char[][] grid, Scanner sc) {
+    private static void processPlayerMove(char[][] grid, Scanner sc, char currPlayer) {
 
         boolean validMove = false;
         while (!validMove) {
@@ -58,22 +69,19 @@ public class TicTacToe {
             } else if (grid[row - 1][col - 1] != '_') {
                 System.out.println("This cell is occupied! Choose another one!");
             } else {
-                grid[row - 1][col - 1] = 'X';
+                grid[row - 1][col - 1] = currPlayer;
                 validMove = true;
             }
         }
     }
 
-    private static String analyzeGame(String gameState) {
+    private static String analyzeGame(char[][] grid) {
 
-        char[][] grid = new char[3][3];
         int xCount = 0, oCount = 0, underscoreCount = 0;
 
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                char c = gameState.charAt(row * 3 + col);
-                grid[row][col] = c;
-
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                char c = grid[row][col];
                 switch (c) {
                     case 'X' -> xCount++;
                     case 'O' -> oCount++;
@@ -87,20 +95,19 @@ public class TicTacToe {
 
         if ((xWins && oWins)
                 || (Math.abs(xCount - oCount) >= 2 && underscoreCount > 0 && !xWins && !oWins)) {
-            return "Impossible";
+            return INVALID_STATE;
         } else if (xWins) {
-            return "X wins";
+            return X_WIN;
         } else if (oWins) {
-            return "O wins";
+            return O_WIN;
         } else if (underscoreCount > 0) {
-            return "Game not finished";
+            return GAME_IN_PROGRESS;
         } else {
-            return "Draw";
+            return DRAW;
         }
     }
 
     private static boolean checkPlayerWins(char[][] grid, char player) {
-
         for (int i = 0; i < 3; i++) {
             if (grid[i][0] == player && grid[i][1] == player && grid[i][2] == player) {
                 return true;
